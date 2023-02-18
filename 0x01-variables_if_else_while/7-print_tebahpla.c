d957da7fbf5312970a6df91d3d54b93b8e7be8a8
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-18 03:12:35 +0200

    Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int a = 'z';
+
+	for (a = 'z'; a >='a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
