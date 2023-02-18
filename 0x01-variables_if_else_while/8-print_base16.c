df55d89ae4709bae84db0ad7d7b122578080425c
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-18 03:47:10 +0200

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	char num = '0';
+	char let = 'a';
+
+	for (num = '0'; num <= '9'; a++)
+	{
+		putchar(a);
+	}
+	for (let = 'a'; let <= 'f'; a++)
+	{
+		putchar(let);
+	}
+	putchar('\n');
+	return (0);
+}
