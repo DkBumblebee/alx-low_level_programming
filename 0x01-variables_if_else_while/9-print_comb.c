91439625da0162b5886baa2b6c4c4942b118242e
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-18 04:27:13 +0200

    It worked

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+			a++;
+		}
+		else
+		{
+			putchar('\n');
+			return (0);
+		}
+	}
+	return (0);
+}
