4b8dad925236ec6d575cbb539034b603d784fee2
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-22 00:47:33 +0200

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _abs - the absolute value of an integer
+ * @n: int to be checked
+ *
+ * Return: Void
+ */
+
+int _abs(int)
+{
+	for (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
