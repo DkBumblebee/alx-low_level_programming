af5fc4be47198dd2567edccc455afa76e1b2cdf9
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-21 20:53:49 +0200

    C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char lett[] = "_putchar";
+
+	int a;
+	for (a = 0; a < 8; a++)
+	{
+		_putchar(lett[a]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,6 @@
+.
+├── main.h
+├── _putchar.c
+└── README.md
+
+1 directory, 3 files
