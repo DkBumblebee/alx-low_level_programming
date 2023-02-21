a7cced0242c5df9d4d8593b49dbed3a8dc04eb0e
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-22 01:49:31 +0200

    C 0x02

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that adds two intergers and return the result
+ * @x: int to be added
+ * @y: int to be added
+ * Return: sum
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
