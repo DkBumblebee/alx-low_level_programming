b04007b2727591d426cdcebe623784388e355eea
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-23 15:40:19 +0200

    what

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_most_numbers - function that prints the numbers, form 0 to 9
- * dose not print @2 and @4
+ * dose not print 2 and 4
  * Return: 0
  */
 
@@ -15,5 +15,5 @@ void print_most_numbers(void)
 		if ((i != 50) && (i != 52))
 			_putchar(i);
 	}
-	_putchar(10);
+	_putchar('\n');
 }
