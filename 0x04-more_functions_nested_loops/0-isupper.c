e24ad3999e8be828e8c025f738b0a1febd127ffc
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-23 14:30:12 +0200

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - functions that checkes for uppercase character
+ * Return 1 if @c is uppercase
+ */
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	return (1);
+
+	return (0);
+}
