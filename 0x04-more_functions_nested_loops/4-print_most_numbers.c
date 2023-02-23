ccbb33ed99cc34ddeedea27c78f2a7230e103ebc
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-23 15:33:18 +0200

    Why no some

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, form 0 to 9
+ * dose not print @2 and @4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 58; i++)
+	{
+		if ((i != 50) && (i != 52))
+			_putchar(i);
+	}
+	_putchar("\n");
+}
