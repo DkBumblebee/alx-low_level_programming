dfcef993bb4ba4c1ca2d4206c45c4777e3ed495c
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-23 16:35:40 +0200

    Dai

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws a diagonal line
+ * @n: parameter
+ * end in a new line
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int lin, new;
+
+	if (n-- > 0)
+	{
+		for (lin = 0; lin < n; lin++)
+		{
+			for (new = 0; new < lin; new++)
+			_putchar(' ');
+			_putchar('\\');
+
+			if (lin == (n - 1))
+				continue;
+				_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
