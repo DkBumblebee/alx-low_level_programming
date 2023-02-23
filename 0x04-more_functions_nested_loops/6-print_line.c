8f4a1998e0b08f2137d7630647244c76532b5714
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-23 16:22:06 +0200

    test

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ * @n: parameter
+ * end in a new line
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		putchar('_');
+	}
+	_putchar('\n');
+}
