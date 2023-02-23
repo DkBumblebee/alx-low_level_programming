d12caa974f06bceac6cf0212968ae155301daed2
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-02-23 14:40:32 +0200

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - functions that checkes for uppercase character
+ * Return: return 1 if uppercase, 0 if not
+ * @c: character to test
+ */
+
+int _isdigit(int c)
+{
+	for ((c >= 0) && (c <= 9))
+	return (1);
+
+	return (0);
+}
