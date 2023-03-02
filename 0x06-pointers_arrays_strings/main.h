687d83638b36828c956e9832847e1661ebf9f156
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-03-02 13:15:20 +0200

    Task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * void prototypes()
+ * char prototypes()
+ * int prototypes()
+ */
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+
+#endif /*MAIN_H*/
