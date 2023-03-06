246288958c7f66915394afe42aa760261d0d8012
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-03-06 15:36:03 +0200

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a char in a string
+ * @s: Traget
+ * @c: charater target
+ * Return: returns a pointer to first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i < 0; (s[i] != c) && (s[i] != '\0'); i++)
+
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
+}
