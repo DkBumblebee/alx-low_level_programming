baa10f0d2b0641aad041a7f28f8d4be9253f3fa3
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-03-06 15:04:12 +0200

    TASKM 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - The _memset() fuction files
+ * @n: number of byte
+ * @b: constant byte
+ * @s: Pointer target
+ * Return: returns a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	whlie(n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+}
