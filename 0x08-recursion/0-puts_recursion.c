16e946bec826a2284c339ddb855ad4b0ac9cdf4d
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-03-08 15:03:48 +0200

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _put_recursion - Function that prints a string, followed by a new line
+ * _puts_recursion - Function that prints a string, followed by a new line
  * @s: Output charater
  * Return: 0
  */
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		_putchar(*s);
-		_putchar_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
