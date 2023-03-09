7b418b1fa1d305f90b3113414d5e27bed453c73b
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-03-09 05:23:28 +0200

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,9 @@ int _sqrt_recursion(int n);
 int _sqrt(int n, int x);
 int is_prime_number(int n);
 int is_prime(int n, int i);
+int is_palindrome(char *s);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int wildcmp(char *s1, char *s2);
 
 #endif
