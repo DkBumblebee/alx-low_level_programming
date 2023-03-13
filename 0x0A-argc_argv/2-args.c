71bcf3e47e53541dd8cdec27539176981523d809
Boitumelo <boitumelogiftmaswanganyi@gmail.com>
2023-03-13 13:42:34 +0200

    Document update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,9 +2,9 @@
 
 /**
  * main - prints argument size
- *@argc: number of arguments
- *@argv: array of arguments type char
- *Return: 0
+ * @argc: number of arguments
+ * @argv: array of arguments type char
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	for (index = 0; index < argc; index++)
 	{
-		printf("%s\n", argv[]);
+		printf("%s\n", argv[index]);
 	}
 	return (0);
 }
